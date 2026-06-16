210d0507b322c85e33b8f9f38ef9483cc51aab37
pun narrans <pun.narrans@gmail.com>
2026-06-16 13:09:03 +0200

    add checks for alpha, email, decimal. typing

--- filter_users.py
+++ filter_users.py
@@ -1,50 +1,83 @@
 """Module to read files"""
 import json
 
-def filter_users_by_name(name, users):
+def filter_users_by_name(name: str, users: list):
     """Filters by name"""
     filtered_users = [user for user in users
                       if user["name"].lower() == name.lower()]
+    
+    if not filtered_users:
+        print("Name not found in users")
+    else:
+        for user in filtered_users:
+            print(user)
 
-    for user in filtered_users:
-        print(user)
-
-def filter_users_by_age(age, users):
+def filter_users_by_age(age: int, users: list):
     """Filters by age"""
-    ## TODO: sanitize input
     filtered_users = [user for user in users
-                      if user.get("age") == int(age)]
+                      if user.get("age") == age]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print("Age not found in users")
+    else:
+        for user in filtered_users:
+            print(user)
 
-def filter_users_by_email(email, users):
+def filter_users_by_email(email: str, users: list):
     """Filters by email"""
     filtered_users = [user for user in users
                       if user.get("email", "").lower() == email.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if not filtered_users:
+        print("Email not found in users")
+    else:
+        for user in filtered_users:
+            print(user)
+
+def check_if_dec(inp: str):
+    """checks if input is decimal"""
+    return inp.isdecimal()
 
 
-def select_filter_by_input(users):
+def check_if_alpha(inp: str):
+    """checks if input is alphabetical"""
+    return inp.isalpha()
+
+def check_if_email(inp: str):
+    """checks if input contains '@' and '.' """
+    return "@" in inp and "." in inp
+
+def select_filter_by_input(users: list):
     """Asks for user input and calls filter fn"""
     prompt = "What would you like to filter by? ('name','age', 'email' supported): "
     filter_option = input(prompt).strip().lower()
+
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search, users)
+        if check_if_alpha((name_to_search)):
+            filter_users_by_name(name_to_search, users)
+        else:
+            print("Valid input is [a-z]")
+
     elif filter_option == "age":
         age_to_search = input("Enter a age to filter users: ").strip()
-        filter_users_by_age(age_to_search, users)
+        if check_if_dec((age_to_search)):
+            filter_users_by_age(int(age_to_search), users)
+        else:
+            print("Valid input is [0-9]")
+
     elif filter_option == "email":
         email_to_search = input("Enter a email to filter users: ").strip()
-        filter_users_by_email(email_to_search, users)
+        if check_if_email(email_to_search):
+            filter_users_by_email(email_to_search, users)
+        else:
+            print("Email must contain '@‘ and ‘.‘")
     else:
         print("Filtering by that option is not yet supported.")
+        select_filter_by_input(users)
 
 
-def load_user_data():
+def load_user_data() -> list[str | int]:
     """Loads user data from file."""
     try:
         with open("users.json", "r", encoding="utf-8") as file:
