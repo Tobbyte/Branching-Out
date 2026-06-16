26aee54550b7db16d016b51e1d10a082a8e5d99a
pun narrans <pun.narrans@gmail.com>
2026-06-16 11:17:09 +0200

    refactor: seperate logic into select_filter_by_input(), load_user_data(), savely call load data

--- filter_users.py
+++ filter_users.py
@@ -1,18 +1,15 @@
 """Module to read files"""
 import json
 
-def filter_users_by_name(name):
+def filter_users_by_name(name, users):
     """Filters by name"""
-    with open("users.json", "r", encoding="utf-8") as file:
-        users = json.load(file)
-
     filtered_users = [user for user in users
                       if user["name"].lower() == name.lower()]
 
     for user in filtered_users:
         print(user)
 
-def filter_users_by_age(age):
+def filter_users_by_age(age, users):
     """Filters by age"""
     ## TODO: sanitize input
     filtered_users = [user for user in users
@@ -21,11 +18,8 @@ def filter_users_by_age(age):
     for user in filtered_users:
         print(user)
 
-def filter_users_by_email(email):
+def filter_users_by_email(email, users):
     """Filters by email"""
-    with open("users.json", "r", encoding="utf-8") as file:
-        users = json.load(file)
-
     filtered_users = [user for user in users
                       if user.get("email", "").lower() == email.lower()]
 
@@ -33,11 +27,34 @@ def filter_users_by_email(email):
         print(user)
 
 
-if __name__ == "__main__":
-    PROMPT = "What would you like to filter by? ('name','age', 'email' supported): "
-    filter_option = input(PROMPT).strip().lower()
+def select_filter_by_input(users):
+    """Asks for user input and calls filter fn"""
+    prompt = "What would you like to filter by? ('name','age', 'email' supported): "
+    filter_option = input(prompt).strip().lower()
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
+        filter_users_by_name(name_to_search, users)
+    elif filter_option == "age":
+        age_to_search = input("Enter a age to filter users: ").strip()
+        filter_users_by_age(age_to_search, users)
+    elif filter_option == "email":
+        email_to_search = input("Enter a email to filter users: ").strip()
+        filter_users_by_email(email_to_search, users)
     else:
         print("Filtering by that option is not yet supported.")
+
+
+def load_user_data():
+    """Loads user data from file."""
+    try:
+        with open("users.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("ERROR: users.json file not found.")
+        return []
+
+
+if __name__ == "__main__":
+    user_data = load_user_data() ## pass for now, until we have classes
+    if user_data:
+        select_filter_by_input(user_data)
